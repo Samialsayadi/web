42173113a96bd715b0da202153d22ed11348c779
Alex TYRODE <alex.tyrode@outlook.fr>
2025-01-25 21:55:33 +0000

    fix: remove routers/__init__.py and update server routers

--- src/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-""" This module contains the routers for the FastAPI application. """
-
-from routers.dynamic import router as dynamic
-from routers.index import router as index
-
-__all__ = ["dynamic", "index"]
